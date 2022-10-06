aa64ff8f170267a50b06ab7768fc41e122def7ae
JackBilbo <abaelard@gmx.de>
2022-10-06 23:18:29 +0200

    Updated for 2.0.5

--- gotfriends-discus2c-premium-lxn/html_ui/Pages/VCockpit/Instruments/TouchingCloud/LX_MAP/soarnet_class.js
+++ gotfriends-discus2c-premium-lxn/html_ui/Pages/VCockpit/Instruments/TouchingCloud/LX_MAP/soarnet_class.js
@@ -31,6 +31,10 @@ class soarnet {
     }
 
     updateUserdata() {
+        let taskstate = "not started";
+        if(B21_SOARING_ENGINE.task_started()) { taskstate = "started" }
+        if(B21_SOARING_ENGINE.task_finished()) { taskstate = "finished" }
+
         SOARNET.userId = SOARNET.userId == "" ? SOARNET.getUserId() : SOARNET.userId ;
         SOARNET.writeUserData(
             "test", SOARNET.userId, {
@@ -39,7 +43,10 @@ class soarnet {
                 "long":     parseFloat(SimVar.GetSimVarValue("A:PLANE LONGITUDE", "degrees longitude")),
                 "hdg":      this.instrument.vars.hdg.value,
                 "alt":      this.instrument.vars.alt.value,
-                "dist":     B21_SOARING_ENGINE.task.remaining_distance_m(),
+                "dist":     B21_SOARING_ENGINE.task.distance_m() - B21_SOARING_ENGINE.task.remaining_distance_m(),
+                "avg":      B21_SOARING_ENGINE.task.avg_task_speed_kts(),
+                "tasktime": this.instrument.vars.tasktime.value,
+                "taskstate":taskstate,
                 "time":     Math.floor(Date.now() / 1000)
             }
         )
@@ -50,26 +57,42 @@ class soarnet {
 SOARNET.displayUserList = function(){
     let list = document.querySelector("#userlist tbody");
     let userList = [];
+    let finisherlist = [];
     let now = Math.floor(Date.now() / 1000);
     list.innerHTML = "";
     
     for (user in SOARNET.eventusers) {
         if(now - parseInt(SOARNET.eventusers[user].time) < 6000) {
-            userList.push(SOARNET.eventusers[user]);
+            if(SOARNET.eventusers[user].taskstate == "finished") {
+                finisherlist.push(SOARNET.eventusers[user]);
+            } else {
+                userList.push(SOARNET.eventusers[user]);
+            }
+            
             if(typeof(TOPOMAP.addLayer) == "function" && user != this.userId) {
                 NAVMAP.paintMultiplayers(user, SOARNET.eventusers[user]);
             }
-        } else {
-            
-        }      
+        }     
     }
 
+    finisherlist.sort((a,b) => {
+        return parseInt(a.tasktime) - parseInt(b.tasktime);
+      })
+
     userList.sort((a,b) => {
-      return parseInt(a.dist) - parseInt(b.dist);
+      return parseInt(b.dist) - parseInt(a.dist);
     })
 
-    userList.forEach((el, idx) => {
-      list.innerHTML += "<tr><td class='alignright'>" + (idx+1) + "</td><td class='mpusername'>" + el.username + "</td><td>" + LXN.displayValue(el.alt, "ft", "alt") + LXN.units.alt.pref + "</td><td class='alignright'>" + LXN.displayValue(el.dist, "m", "dist") + LXN.units.dist.pref + "</td></tr>";
+    let i = 1;
+
+    finisherlist.forEach((el) => {
+        list.innerHTML += "<tr><td class='alignright'>" + i + "</td><td class='mpusername'>" + el.username + "</td><td>" + LXN.displayValue(el.alt, "ft", "alt") + "</td><td class='alignright'>" + LXN.displayValue(el.avg, "kts", "speed") + "</td><td class='alignright'>" + LXN.displayValue(el.tasktime,"s","time_of_day") + "</td></tr>";
+        i++;
+    })
+
+    userList.forEach((el) => {
+      list.innerHTML += "<tr><td class='alignright'>" + (el.taskstate == "not started" ? "--" : i) + "</td><td class='mpusername'>" + el.username + "</td><td>" + LXN.displayValue(el.alt, "ft", "alt") + "</td><td class='alignright'>" + LXN.displayValue(el.avg, "kts", "speed") + "</td><td class='alignright'>" + (el.taskstate == "not started" ? "0" : LXN.displayValue(el.dist, "m", "dist")) + "</td></tr>";
+      i++;  
     })
   }
 
