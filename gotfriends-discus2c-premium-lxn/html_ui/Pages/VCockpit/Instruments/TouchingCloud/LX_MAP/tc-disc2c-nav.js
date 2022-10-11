11bc8397c3f1c29170e1f08516011b2c18fd6c10
JackBilbo <abaelard@gmx.de>
2022-10-11 22:20:35 +0200

    Countdown & Labels for rangeinput

--- gotfriends-discus2c-premium-lxn/html_ui/Pages/VCockpit/Instruments/TouchingCloud/LX_MAP/tc-disc2c-nav.js
+++ gotfriends-discus2c-premium-lxn/html_ui/Pages/VCockpit/Instruments/TouchingCloud/LX_MAP/tc-disc2c-nav.js
@@ -289,7 +289,6 @@ class lxn extends NavSystemTouch {
             CONFIGPANEL.update();
             this.updateKineticAssistant();
 
-            SN.update();
         }
 
         if(this.TIME_S - this.TIMER_1 > 1) {
@@ -301,6 +300,7 @@ class lxn extends NavSystemTouch {
             if(this.vars.utctime.isUsed) {this.vars.utctime.value = new Date().toUTCString().replace(/.*(\d\d:\d\d:\d\d).*/,"$1"); }
 
             this.updateLiftdots();
+            SN.update();
 
             if(this.vars.alt_gnd.value > 100) { this.log.isStarted = true; }
             if(this.vars.gndspd.value < 20 && this.log.isStarted == true) { this.log.isStarted = false; }
