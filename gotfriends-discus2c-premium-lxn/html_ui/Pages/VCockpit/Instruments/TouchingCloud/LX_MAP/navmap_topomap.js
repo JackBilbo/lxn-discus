c8b7b5baea2d78c9d0ee1b4c4ea7ee172869c75e
JackBilbo <abaelard@gmx.de>
2022-10-22 22:23:23 +0200

    Bugfixes SSC Saturday

--- gotfriends-discus2c-premium-lxn/html_ui/Pages/VCockpit/Instruments/TouchingCloud/LX_MAP/navmap_topomap.js
+++ gotfriends-discus2c-premium-lxn/html_ui/Pages/VCockpit/Instruments/TouchingCloud/LX_MAP/navmap_topomap.js
@@ -477,9 +477,9 @@ class navmap {
         }
 
         for (var marker in this.mpMarker) {
-            if(!SOARNET.eventusers[marker]) {
+            if(!SOARNET.eventusers[marker] && this.mpMarker[marker] != null) {
                 TOPOMAP.removeLayer(this.mpMarker[marker]);
-                this.mpMarker[marker] = null;
+                delete this.mpMarker[marker];
             }
         }
     }
@@ -487,8 +487,8 @@ class navmap {
     wipeMultiplayers() {
         for (var marker in this.mpMarker) {
             TOPOMAP.removeLayer(this.mpMarker[marker]);
-            this.mpMarker[marker] = null;
         }
+        this.mpMarker = {}
     }
 
     initMap() {
